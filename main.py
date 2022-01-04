aba89166f56c9725be32ee6f544755faa51b215c
Tom Planche <58936594+TomPlanche@users.noreply.github.com>
2022-01-04 16:28:51 +0100

    test commit

---
+++ main.py
@@ -0,0 +1,71 @@
+
+from copy import deepcopy
+
+
+def reversedTom(liste: list[int]):
+    liste.reverse()
+    return liste
+
+
+def remontee(Matrice: list[list[int]], Colonne: list[int]) -> list[float]:
+    z = Colonne[-1] / Matrice[-1][-1]
+    y = Colonne[-2] * -1 - z
+    x = Colonne[-3] - Matrice[-3][-2] * y - Matrice[-3][-1] * z
+
+    # test = [
+    #     Colonne[]
+    # ]
+    Matrice.reverse()
+    print([(Colonne[-(i+1)] / reversedTom(elem)[:i+1][0]) for i, elem in enumerate(Matrice)])
+    a = [x, y, z]
+    a.reverse()
+    return a
+
+
+def permute(matrice1: list[list[int]], matrice2: list[list[int]], indexLigne1: int,  indexLigne2: int)\
+        -> tuple[list[list[int]], list[list[int]]]:
+    """
+    Permute deux lignes d'index choisi de deux matrices
+    :param matrice1:
+    :param matrice2:
+    :param indexLigne1:
+    :param indexLigne2:
+    :return:
+    """
+    nouvelleMatrice1 = deepcopy(matrice1)
+    nouvelleMatrice2 = deepcopy(matrice2)
+
+    nouvelleMatrice1[indexLigne1] = matrice2[indexLigne2]
+    nouvelleMatrice2[indexLigne2] = matrice1[indexLigne1]
+
+    return nouvelleMatrice1, nouvelleMatrice2
+
+
+if __name__ == '__main__':
+    # A = [
+    #     [1, 3, 2],
+    #     [0, -1, -1],
+    #     [0, 0, 3]
+    # ]
+    #
+    # B = [64, -24, 31]
+    #
+    # print(remontee(A, B))
+
+    m1 = [
+        [1, 2, 3],
+        [13, 14, 15],
+        [7, 8, 9]
+    ]
+
+    m2 = [
+        [10, 11, 12],
+        [4, 5, 6],
+        [16, 17, 18]
+    ]
+
+    m1, m2 = permute(m1, m2, 1, 1)
+
+    print(m1)
+    print(m2)
+
